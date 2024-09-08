138831455a94e8634f448a8ee42db895190a3df6
qx3kk <romanchuk.up@gmail.com>
2024-09-08 21:50:04 +0300

    rename: змінив назви файлів з word_trainer на vocab_trainer

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -5,10 +5,10 @@ def register_handlers(dp: Dispatcher) -> None:
     """Реєструємо усі хендлери"""
     from . import (menu,
                    help,
-                   dict_base,
+                   vocab_base,
                    word_trainer)
 
     dp.include_router(menu.router)
     dp.include_router(help.router)
-    dp.include_router(dict_base.router)
+    dp.include_router(vocab_base.router)
     dp.include_router(word_trainer.router)
