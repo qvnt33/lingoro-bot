2a4e2acb418e4b9dd364c2a169c910e2723c0f06
qx3kk <romanchuk.up@gmail.com>
2024-09-08 21:00:58 +0300

    refactor: handlers/help.py почистив код

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -1,10 +1,8 @@
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
+from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
 from src.keyboards.help_kb import get_inline_kb_help
-
-from db.database import Base, Session, engine
-from db.models import WordPair, User, Dictionary
 
 from tools.read_data import data
 
@@ -13,7 +11,8 @@ router = Router()
 
 @router.callback_query(F.data == 'help')
 async def process_btn_help(callback: CallbackQuery) -> None:
-    with Session as db:
-        text_help = data['prompt']['text_help']
-        await callback.message.edit_text(text=text_help,
-                                         reply_markup=get_inline_kb_help())
+    text_help: str = data['prompt']['text_help']
+    kb: InlineKeyboardMarkup = get_inline_kb_help()
+
+    await callback.message.edit_text(text=text_help,
+                                     reply_markup=kb)
