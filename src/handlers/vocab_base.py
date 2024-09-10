47577604aa4b6b4a44053f32da7f3cddf3fe3555
qx3kk <romanchuk.up@gmail.com>
2024-09-11 00:30:37 +0300

    refactor: corrected pep8 handlers/vocab_base.py

--- src/handlers/vocab_base.py
+++ src/handlers/vocab_base.py
@@ -1,14 +1,12 @@
 from aiogram import F, Router
+from aiogram.enums import ParseMode
 from aiogram.types import CallbackQuery
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
-
 from sqlalchemy.orm.query import Query
 
 from db.database import Session
 from db.models import User, Vocabulary
-
 from src.keyboards.vocab_base_kb import get_inline_kb_user_vocabs
-
 from tools.read_data import app_data
 
 router = Router()
@@ -16,14 +14,14 @@ router = Router()
 
 @router.callback_query(F.data == 'vocab_base')
 async def process_btn_vocab_base(callback: CallbackQuery) -> None:
-    """Відстежує натискання на кнопку "База словників".
+    """Відстежує натискання на кнопку бази словників.
     Відправляє користувачу список його словників.
     """
     with Session as db:
         # Отримання усіх словників, фільтруючи їх по user_id користувача
         user_vocabs: Query[Vocabulary] = db.query(Vocabulary).filter(User.id == Vocabulary.user_id)
 
-        # Прапорець, чи порожня база словників користувача
+        # Флаг, чи порожня база словників користувача
         is_vocab_base_empty: bool = len(user_vocabs.all()) == 0
 
         kb: InlineKeyboardMarkup = get_inline_kb_user_vocabs(user_vocabs=user_vocabs,
@@ -33,18 +31,19 @@ async def process_btn_vocab_base(callback: CallbackQuery) -> None:
 
     # Якщо база словників порожня
     if is_vocab_base_empty:
-        msg_vocab_base = app_data['handlers']['vocab_base']['vocab_base_is_empty']
+        msg_vocab_base: str = app_data['handlers']['vocab_base']['vocab_base_is_empty']
     else:
-        msg_vocab_base = app_data['handlers']['vocab_base']['msg_select_vocab']
+        msg_vocab_base: str = app_data['handlers']['vocab_base']['msg_select_vocab']
 
     await callback.message.edit_text(text=msg_vocab_base,
-                                     reply_markup=kb)
+                                     reply_markup=kb,
+                                     parse_mode=ParseMode.HTML)
 
 
 """
 @router.callback_query(F.data == 'dict_add')
 async def process_btn_dict_add(callback: CallbackQuery):
-    """ """
+
     text_dict_add = 'Введите название нового словаря'
     await callback.message.edit_text(text=text_dict_add)
 
