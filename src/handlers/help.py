5ff47a945f2545d0693139ec81c8b86f3d94ed7c
qx3kk <romanchuk.up@gmail.com>
2024-09-10 23:43:26 +0300

    refactor: corrected pep8 handlers/help.py

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -1,9 +1,9 @@
-from aiogram import Router, F
+from aiogram import F, Router
+from aiogram.enums import ParseMode
 from aiogram.types import CallbackQuery
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
 from src.keyboards.help_kb import get_inline_kb_help
-
 from tools.read_data import app_data
 
 router = Router()
@@ -11,8 +11,9 @@ router = Router()
 
 @router.callback_query(F.data == 'help')
 async def process_btn_help(callback: CallbackQuery) -> None:
-    text_help: str = app_data['handlers']['help']['title_help']
+    title_help: str = app_data['handlers']['help']['title_help']
     kb: InlineKeyboardMarkup = get_inline_kb_help()
 
-    await callback.message.edit_text(text=text_help,
-                                     reply_markup=kb)
+    await callback.message.edit_text(text=title_help,
+                                     reply_markup=kb,
+                                     parse_mode=ParseMode.HTML)
