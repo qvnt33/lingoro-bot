6490d01a599fcbec1e0f4e8e09211a0bdc29de00
qx3kk <romanchuk.up@gmail.com>
2024-09-26 00:15:16 +0300

    chore: added constant vocab_pagination_limit to config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 TOKEN = os.getenv('TOKEN')
+VOCAB_PAGINATION_LIMIT = 10  # Ліміт кількості кнопок словників на сторінці пагінації
