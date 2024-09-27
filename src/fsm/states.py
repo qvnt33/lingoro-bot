5c17d436196ef4465fde51445dc5e37f76fffd24
qx3kk <romanchuk.up@gmail.com>
2024-09-28 00:39:26 +0300

    feature: created fsm category

---
+++ src/fsm/states.py
@@ -0,0 +1,7 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class VocabCreation(StatesGroup):
+    waiting_for_vocab_name = State()  # Стан очікування назви словника
+    waiting_for_vocab_note = State()  # Стан очікування примітки до словника
+    waiting_for_vocab_wordpairs = State()  # Стан очікування словникових пар
