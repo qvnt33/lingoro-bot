a54ddc995c5d8ffae792875f9e243aa11b065ed3
qx3kk <romanchuk.up@gmail.com>
2024-09-28 01:48:42 +0300

    chore: added fsm to fsm dir

--- src/fsm/states.py
+++ src/fsm/states.py
@@ -4,4 +4,4 @@ from aiogram.fsm.state import State, StatesGroup
 class VocabCreation(StatesGroup):
     waiting_for_vocab_name = State()  # Стан очікування назви словника
     waiting_for_vocab_note = State()  # Стан очікування примітки до словника
-    waiting_for_vocab_wordpairs = State()  # Стан очікування словникових пар
+    waiting_for_wordpairs = State()  # Стан очікування словникових пар
