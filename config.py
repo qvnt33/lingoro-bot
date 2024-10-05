321db16d858cf3be8be9e9091cece11e26e4235b
qx3kk <romanchuk.up@gmail.com>
2024-10-05 19:21:28 +0300

    feat: зробити більш гарною структуру констант

--- config.py
+++ config.py
@@ -4,18 +4,27 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-TOKEN = os.getenv('TOKEN')
-VOCAB_PAGINATION_LIMIT = 10  # Ліміт кількості кнопок словників на сторінці пагінації
+TOKEN = os.getenv('TOKEN')  # Токен Телеграм
 
-MIN_LENGTH_VOCAB_NAME = 3
-MAX_LENGTH_VOCAB_NAME = 50
+DEFAULT_VOCAB_NOTE = 'Немає'  # Стандартна назва примітки до словника
+WORDPAIR_SEPARATOR = ':'  # Символ, за допомогою якого будуть розділяти словникові пари
 
-MIN_LENGTH_VOCAB_NOTE = 3
-MAX_LENGTH_VOCAB_NOTE = 100
+VOCAB_PAGINATION_LIMIT = 10  # Кількість кнопок словників на сторінці пагінації
 
-DEFAULT_VOCAB_NOTE = 'Немає'
+MIN_LENGTH_VOCAB_NAME = 3  # Мінімальна к-сть символів у "назві" словника
+MAX_LENGTH_VOCAB_NAME = 50  # Максимальна к-сть символів у "назві" словника
 
-MAX_COUNT_WORDS_WORDPAIR = 3
-MAX_COUNT_TRANSLATIONS_WORDPAIR = 3
-MAX_LENGTH_WORD_WORDPAIR = 30
-MAX_LENGTH_TRANSLATION_WORDPAIR = 30
+MIN_LENGTH_VOCAB_NOTE = 3  # Мінімальна кількість символів у "примітці" до словника
+MAX_LENGTH_VOCAB_NOTE = 100  # Максимальна кількість символів у "примітці" до словника
+
+MIN_COUNT_WORDS_WORDPAIR = 1  # Мінімальна кількість "слів" у словниковій парі
+MAX_COUNT_WORDS_WORDPAIR = 3  # Максимальна кількість "слів" у словниковій парі
+
+MIN_COUNT_TRANSLATIONS_WORDPAIR = 1  # Мінімальна кількість "перекладів" у словниковій парі
+MAX_COUNT_TRANSLATIONS_WORDPAIR = 3  # Максимальна кількість "перекладів" у словниковій парі
+
+MIN_LENGTH_WORD_WORDPAIR = 1  # Мінімальна кількість "слів" у одній словниковій парі
+MAX_LENGTH_WORD_WORDPAIR = 30  # Максимальна кількість "слів" у одній словниковій парі
+
+MIN_LENGTH_TRANSLATION_WORDPAIR = 1  # Мінімальна кількість "перекладів" у одній словниковій парі
+MAX_LENGTH_TRANSLATION_WORDPAIR = 30  # Максимальна кількість "перекладів" у одній словниковій парі
