3ab8abe37106b61a0f566744c6bb5d4b25ef7ebe
qx3kk <romanchuk.up@gmail.com>
2024-10-06 23:22:50 +0300

    feat: змінити логіку форматування повідомлення

--- tools/message_formatter.py
+++ tools/message_formatter.py
@@ -1,8 +1,13 @@
-def format_message(vocab_name: str = 'Відсутня', vocab_note: str = 'Відсутня', content: str = '') -> str:
+def format_message(vocab_name: str = None, vocab_note: str = None, content: str = '') -> str:
     """Форматує повідомлення бота з заголовком і основним контентом"""
+    # Якщо vocab_name або vocab_note порожні, використовуємо значення за замовчуванням
+    vocab_name = vocab_name or 'Відсутня'
+    vocab_note = vocab_note or 'Відсутня'
+
+    # Форматуємо повідомлення
     formatted_message: str = (
         f'Назва: {vocab_name}\n'
         f'Примітка: {vocab_note}\n\n'
         f'{content}'
-)
+    )
     return formatted_message
