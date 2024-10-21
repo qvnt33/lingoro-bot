a46ac94bbeb984391d3a46dfb2807c1c90797d87
qx3kk <romanchuk.up@gmail.com>
2024-10-21 03:54:40 +0300

    chore: додаnb текст до розділу допомоги

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -3,7 +3,7 @@ from aiogram.types import CallbackQuery
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
 from src.keyboards.help_kb import get_inline_kb_help
-from tools.read_data import app_data
+from text_data import MSG_HELP
 
 router = Router()
 
@@ -13,7 +13,7 @@ async def process_btn_help(callback: CallbackQuery) -> None:
     """Відстежує натискання на кнопку допомоги".
     Відправляє користувачу повідомлення з допомогою.
     """
-    title_help: str = app_data['help']['help']
+    title_help: str = MSG_HELP
     kb: InlineKeyboardMarkup = get_inline_kb_help()
 
     await callback.message.edit_text(text=title_help,
