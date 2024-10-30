367554f9a595ff47634b20dae813a907b486287e
qx3kk <romanchuk.up@gmail.com>
2024-10-30 16:33:38 +0200

    refactor: видалити зайві коментарі з bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,27 @@
-import asyncio  # noqa: I001
-import logging  # Імпорт логування
+import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
-from aiogram.client.bot import DefaultBotProperties
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from config import TOKEN
 from db.models import create_tables
-from logging_config import setup_logging  # Імпорт налаштувань логування
+from logging_config import setup_logging
 from src.handlers import register_handlers
 
 
 async def main() -> None:
-    setup_logging()  # Налаштування логування на самому початку програми
-    logging.info('Запуск бота')
+    setup_logging()
+    create_tables()
 
-    create_tables()  # Створення всіх таблиць
-
-    bot = Bot(token=TOKEN)
-    # default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN)
+    bot = Bot(token=TOKEN,
+              default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
     dp = Dispatcher()
 
-    register_handlers(dp)  # Підключення роутерів
+    register_handlers(dp)
 
-    await bot.delete_webhook(drop_pending_updates=True)
-
+    logging.info('Запуск бота')
     await dp.start_polling(bot)
 
 
