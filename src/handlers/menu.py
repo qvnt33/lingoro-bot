de71cae3c9f434541e754c1bff922704b0e0cbb8
qx3kk <romanchuk.up@gmail.com>
2024-10-31 15:54:02 +0200

    refactor: видалити зайвого коду з handler/menu.py

--- src/handlers/menu.py
+++ src/handlers/menu.py
@@ -16,20 +16,19 @@ router = Router(name='menu')
 
 @router.message(Command(commands=['start', 'menu']))
 async def cmd_menu(message: Message) -> None:
-    logging.info(f'Користувач ввів команду "/{message.text}" для переходу до "Головного меню"')
+    logging.info(f'Користувач ввів команду "{message.text}" для переходу до "Головного меню"')
 
     tg_user_data: User | None = message.from_user
 
     kb: InlineKeyboardMarkup = get_inline_kb_menu()
 
-    with Session() as db:
-        if crud.is_user_exists_in_db(db=db, user_id=tg_user_data.id):
+    with Session() as session:
+        if crud.is_user_exists_in_db(session=session, user_id=tg_user_data.id):
             title_menu: str = MSG_MENU
         else:
             title_menu: str = MSG_MENU_FOR_NEW_USER
-            crud.create_user_in_db(db, tg_user_data)
+            crud.create_user_in_db(session, tg_user_data)
             logging.info(f'ДО БД був доданий користувач: {tg_user_data.id}')
-            db.commit()
 
     await message.answer(text=title_menu, reply_markup=kb)
 
@@ -38,17 +37,7 @@ async def cmd_menu(message: Message) -> None:
 async def process_btn_menu(callback: CallbackQuery) -> None:
     logging.info('Користувач натиснув на кнопку для переходу до "Головного меню"')
 
-    tg_user_data: User | None = callback.from_user
-
     kb: InlineKeyboardMarkup = get_inline_kb_menu()
-
-    with Session() as db:
-        if crud.is_user_exists_in_db(db, user_id=tg_user_data.id):
-            title_menu: str = MSG_MENU
-        else:
-            title_menu: str = MSG_MENU_FOR_NEW_USER
-            crud.create_user_in_db(db, tg_user_data)
-            logging.info(f'ДО БД був доданий користувач: {tg_user_data.id}')
-            db.commit()
+    title_menu: str = MSG_MENU
 
     await callback.message.edit_text(text=title_menu, reply_markup=kb)
