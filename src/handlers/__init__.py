c1e7499fef5e0dfb17d0878f6ebac43fd0b067ad
qx3kk <romanchuk.up@gmail.com>
2024-11-02 03:17:03 +0200

    refactor: рефакторинг __init__.py у handlers

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,14 +1,13 @@
-from aiogram import Dispatcher
-
 from . import create_vocab
+from aiogram import Dispatcher
 
 
 def register_handlers(dp: Dispatcher) -> None:
-    """Реєструємо усі хендлери"""
+    """Реєструє усі хендлери"""
     from . import help, menu, vocab_base, vocab_trainer
 
     dp.include_router(menu.router)
     dp.include_router(help.router)
     dp.include_router(vocab_base.router)
-    dp.include_router(vocab_trainer.router)
     dp.include_router(create_vocab.router)
+    dp.include_router(vocab_trainer.router)
