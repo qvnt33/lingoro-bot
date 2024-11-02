fc6296f3c926638036804e3b59736471db499048
qx3kk <romanchuk.up@gmail.com>
2024-11-02 03:16:10 +0200

    refactor: змініти назву методів у fsm/states.py

--- src/fsm/states.py
+++ src/fsm/states.py
@@ -3,7 +3,7 @@ from aiogram.fsm.state import State, StatesGroup
 
 class VocabCreation(StatesGroup):
     waiting_for_vocab_name = State()  # Стан очікування назви словника
-    waiting_for_vocab_note = State()  # Стан очікування примітки до словника
+    waiting_for_vocab_description = State()  # Стан очікування примітки до словника
     waiting_for_wordpairs = State()  # Стан очікування словникових пар
 
 
