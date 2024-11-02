1e31d9780315c600fd2623298e6ac0ea5e1abfd9
qx3kk <romanchuk.up@gmail.com>
2024-11-02 09:57:23 +0200

    refactor: рефакторинг

--- tools/message_formatter.py
+++ tools/message_formatter.py
@@ -1,13 +1,13 @@
 def add_vocab_data_to_message(vocab_name: str = None,
                               vocab_description: str = None,
-                              message: str = '') -> str:
+                              message_text: str = '') -> str:
     """Додає до повідомлення назву та опис словника"""
     vocab_name = vocab_name or 'Відсутня'
-    vocab_description = vocab_description or 'Відсутня'
+    vocab_description = vocab_description or 'Відсутній'
 
     formatted_message: str = (
         f'Назва: {vocab_name}\n'
         f'Опис: {vocab_description}\n\n'
-        f'{message}'
+        f'{message_text}'
     )
     return formatted_message
