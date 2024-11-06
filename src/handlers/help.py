16dba6483208b73043d01ccda0ec026523a6270a
qx3kk <romanchuk.up@gmail.com>
2024-11-06 17:03:10 +0200

    refactor: незначні зміни неймінгу та коментарів

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -3,14 +3,14 @@ from aiogram.types import CallbackQuery
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
 from src.keyboards.help_kb import get_inline_kb_help
-from text_data import MSG_HELP
+from text_data import MSG_TITLE_HELP
 
 router = Router()
 
 
 @router.callback_query(F.data == 'help')
 async def process_btn_help(callback: CallbackQuery) -> None:
-    title_help: str = MSG_HELP
+    title_help: str = MSG_TITLE_HELP
     kb: InlineKeyboardMarkup = get_inline_kb_help()
 
     await callback.message.edit_text(text=title_help,
--- src/handlers/menu.py
+++ src/handlers/menu.py
@@ -9,7 +9,7 @@ from db import crud
 from db.database import Session
 from db.models import User
 from src.keyboards.menu_kb import get_inline_kb_menu
-from text_data import MSG_MENU, MSG_MENU_FOR_NEW_USER
+from text_data import MSG_TITLE_MENU, MSG_TITLE_MENU_FOR_NEW_USER
 
 router = Router(name='menu')
 
@@ -24,9 +24,9 @@ async def cmd_menu(message: Message) -> None:
 
     with Session() as session:
         if crud.is_user_exists_in_db(session=session, user_id=tg_user_data.id):
-            title_menu: str = MSG_MENU
+            title_menu: str = MSG_TITLE_MENU
         else:
-            title_menu: str = MSG_MENU_FOR_NEW_USER
+            title_menu: str = MSG_TITLE_MENU_FOR_NEW_USER
             crud.create_user_in_db(session, tg_user_data)
             logging.info(f'ДО БД був доданий користувач: {tg_user_data.id}')
 
@@ -38,6 +38,6 @@ async def process_btn_menu(callback: CallbackQuery) -> None:
     logging.info('Користувач натиснув на кнопку для переходу до "Головного меню"')
 
     kb: InlineKeyboardMarkup = get_inline_kb_menu()
-    title_menu: str = MSG_MENU
+    title_menu: str = MSG_TITLE_MENU
 
     await callback.message.edit_text(text=title_menu, reply_markup=kb)
