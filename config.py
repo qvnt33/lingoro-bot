eb85fb95fb6e77e2cd7996233d5487b69a851d9b
qx3kk <romanchuk.up@gmail.com>
2024-11-06 17:00:17 +0200

    refactor: відсортувати та відрефакторити константні файли

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-TOKEN = os.getenv('TOKEN')  # Токен API Telegram
+TOKEN: str | None = os.getenv('TOKEN')  # Токен API Telegram
 DATABASE_URL = 'sqlite:///database.db'
 
 # Налаштування словника
 WORDPAIR_SEPARATOR = ':'  # Символ, який використовується для розділення словникових пар
-ITEM_SEPARATOR = ','  # Символ, який використовується для розділення елементів (слів або перекладів)
-TRANSCRIPTION_SEPARATOR = '|'  # Символ, який використовується для розділення слова та транскрипції
-ALLOWED_CHARS: tuple[str] = ('-', '_', ' ')  # Дозволені символи для назви словника та словникових пар
+WORDPAIR_ITEM_SEPARATOR = ','  # Символ, який використовується для розділення елементів (слів або перекладів)
+WORDPAIR_TRANSCRIPTION_SEPARATOR = '|'  # Символ, який використовується для розділення слова та транскрипції
+ALLOWED_CHARS = '-_ '  # Дозволені символи для назви словника та словникових пар
 
 # Пагінація
-VOCAB_PAGINATION_LIMIT = 10  # Кількість кнопок словників на сторінці пагінації
+del_VOCAB_PAGINATION_LIMIT = 10  # Кількість кнопок словників на сторінці пагінації
 
 # Довжина назви словника
 MIN_LENGTH_VOCAB_NAME = 3  # Мінімальна кількість символів у "назві словника"
@@ -25,13 +25,9 @@ MIN_LENGTH_VOCAB_DESCRIPTION = 3  # Мінімальна кількість си
 MAX_LENGTH_VOCAB_DESCRIPTION = 100  # Максимальна кількість символів у "примітці до словника"
 
 # Кількість слів у словниковій парі
-MIN_COUNT_WORDS_WORDPAIR = 1  # Мінімальна кількість "слів"
-MAX_COUNT_WORDS_WORDPAIR = 30  # Максимальна кількість "слів"
+MIN_COUNT_WORDPAIR_ITEMS = 1  # Мінімальна кількість "слів"
+MAX_COUNT_WORDPAIR_ITEMS = 30  # Максимальна кількість "слів"
 
 # Довжина слів в словниковій парі
-MIN_LENGTH_WORD_WORDPAIR = 1  # Мінімальна кількість символів
-MAX_LENGTH_WORD_WORDPAIR = 30  # Максимальна кількість символів
-
-# Довжина анотації в словниковій парі
-MIN_LENGTH_ANNOTATION_WORDPAIR = 1  # Мінімальна кількість символів
-MAX_LENGTH_ANNOTATION_WORDPAIR = 30  # Максимальна кількість символів
+MIN_LENGTH_WORDPAIR_COMPONENT = 1  # Мінімальна кількість символів
+MAX_LENGTH_WORDPAIR_COMPONENT = 30  # Максимальна кількість символів
