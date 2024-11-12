47a75c7621cf82a4bfe189b0da6d8ef1e7b97bab
qx3kk <romanchuk.up@gmail.com>
2024-11-12 19:06:43 +0200

    refactor: додай docstring до функції add_vocab_data_to_message

--- tools/message_formatter.py
+++ tools/message_formatter.py
@@ -1,7 +1,21 @@
 def add_vocab_data_to_message(vocab_name: str | None = None,
                               vocab_description: str | None = None,
                               message_text: str = '') -> str:
-    """Додає до повідомлення назву та опис словника"""
+    """Додає до повідомлення назву та опис словника.
+
+    Notes:
+        - якщо не буде передана назва словника, то записується слово "Відсутня".
+        - якщо не буде переданий опис словника, то записується слово "Відсутній".
+
+    Args:
+        vocab_name (str | None): Назва словника (за замовчуванням: None).
+        vocab_description (str | None): Опис словника (за замовчуванням: None).
+        message_text (str): Повідомлення, до якого потрібно додати інформацію про словник
+        (за замовчуванням пустий рядок).
+
+    Returns:
+        str: Відредаговане повідомлення з доданою назвою та описом словника.
+    """
     vocab_name = vocab_name or 'Відсутня'
     vocab_description = vocab_description or 'Відсутній'
 
