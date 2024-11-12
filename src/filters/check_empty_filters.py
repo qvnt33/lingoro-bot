c6882c189cd037de5f37f762327399c036f86367
qx3kk <romanchuk.up@gmail.com>
2024-11-12 21:19:49 +0200

    refactor: зміни фільтр та його функціонал з not_empty_filter на check_empty_filter

--- src/filters/check_empty_filters.py
+++ src/filters/check_empty_filters.py
@@ -3,13 +3,13 @@ from typing import Iterable
 from .base_filter import BaseFilter
 
 
-class NotEmptyFilter(BaseFilter):
-    """Фільтр для перевірки, що значення не пусте"""
+class CheckEmptyFilter(BaseFilter):
+    """Фільтр для перевірки, що значення пусте"""
 
     def apply(self, value: Iterable | None) -> bool:
         if value is None:
-            is_valid = False
+            is_valid = True
         else:
             current_length: int = len(value)
-            is_valid: bool = current_length > 0
+            is_valid: bool = current_length < 0
         return is_valid
