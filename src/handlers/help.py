e880c8d7d9458ce198814378f69386e4a9bf5fef
qx3kk <romanchuk.up@gmail.com>
2024-11-15 23:16:08 +0200

    refactor: створи хендлер для відстеження команди /help

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -1,10 +1,30 @@
+import logging
+
 from aiogram import F, Router, types
+from aiogram.filters import Command
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
 from src.keyboards.help_kb import get_inline_kb_help
 from text_data import MSG_TITLE_HELP
 
-router = Router()
+router = Router(name='help')
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+@router.message(Command(commands=['help']))
+async def cmd_menu(message: types.Message) -> None:
+    """Відстежує введення команди "help".
+    Перенаправляє до розділу "Довідка".
+    """
+    user_id: int = message.from_user.id
+
+    logger.info(f'Користувач ввів команду "{message.text}". USER_ID: {user_id}')
+    logger.info('Користувач перейшов до розділу "Довідка"')
+
+    kb: InlineKeyboardMarkup = get_inline_kb_help()
+    msg_help_info: str = MSG_TITLE_HELP
+
+    await message.answer(text=msg_help_info, reply_markup=kb)
 
 
 @router.callback_query(F.data == 'help')
@@ -12,7 +32,12 @@ async def process_btn_help(callback: types.CallbackQuery) -> None:
     """Відстежує натискання на кнопку "Довідка" у головному меню.
     Відправляє повідомлення з інструкціями.
     """
-    kb: InlineKeyboardMarkup = get_inline_kb_help()
-    msg_text: str = MSG_TITLE_HELP
+    user_id: int = callback.from_user.id
 
-    await callback.message.edit_text(text=msg_text, reply_markup=kb)
+    logger.info(f'Користувач натиснув на кнопку "Довідка". USER_ID: {user_id}')
+    logger.info('Користувач перейшов до розділу "Довідка"')
+
+    kb: InlineKeyboardMarkup = get_inline_kb_help()
+    msg_help_info: str = MSG_TITLE_HELP
+
+    await callback.message.edit_text(text=msg_help_info, reply_markup=kb)
