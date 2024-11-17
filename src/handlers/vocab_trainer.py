c1d6f8666310e738bc93009ec36b72af04642c80
qx3kk <romanchuk.up@gmail.com>
2024-11-17 22:59:47 +0200

    refactor: pep8

--- src/handlers/vocab_trainer.py
+++ src/handlers/vocab_trainer.py
@@ -6,11 +6,10 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, InlineKeyboardMarkup, Message
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
-from db.crud import get_user_vocab_by_user_id, get_wordpairs_by_vocab_id
+from db.crud import get_wordpairs_by_vocab_id
 from db.database import Session
 from db.models import User
 from src.fsm.states import VocabTraining
-from src.keyboards.vocab_base_kb import get_inline_kb_vocab_selection
 from src.keyboards.vocab_trainer_kb import get_inline_kb_all_training, get_inline_kb_process_training
 from text_data import MSG_ERROR_VOCAB_BASE_EMPTY
 
@@ -23,6 +22,7 @@ async def process_vocab(callback: CallbackQuery, state: FSMContext) -> None:
     user_id: int = callback.from_user.id
 
     with Session() as db:
+        get_user_vocab_by_user_id = 1 # !trash
         vocab_details: dict = get_user_vocab_by_user_id(db, user_id)
         user_vocabs: User | None = get_user_vocab_by_user_id(db, user_id, is_all=True)  # Словники користувача
 
@@ -35,12 +35,12 @@ async def process_vocab(callback: CallbackQuery, state: FSMContext) -> None:
             msg_finally: str = f'Ви обрали словник: "{vocab_name}"\nОберіть тип, будь-ласка, тип тренування.'
 
     # Клавіатура для відображення словників
-    kb: InlineKeyboardMarkup = get_inline_kb_vocab_selection(user_vocabs, is_with_create_vocab=False)
+    # kb: InlineKeyboardMarkup = get_inline_kb_vocab_selection(user_vocabs, is_with_create_vocab=False)
 
     current_state = 'VocabTrainState'  # Стан FSM
     await state.update_data(current_state=current_state)  # Збереження поточного стану FSM
 
-    await callback.message.edit_text(text=msg_finally, reply_markup=kb)
+    # await callback.message.edit_text(text=msg_finally, reply_markup=kb)
 
 
 @router.callback_query(F.data == 'reverse_translation')
@@ -169,3 +169,53 @@ async def process_cancel_training(callback: CallbackQuery, state: FSMContext) ->
     await state.clear()  # Очищення FSM-кеш та FSM-стану
     await state.update_data(vocab_id=vocab_id)  # Оновлення індексу
     await process_vocab(callback, state)
+
+
+
+'''
+@router.callback_query(F.data.startswith('select_vocab_training'))
+async def process_training_vocab_selection(callback: types.CallbackQuery, state: FSMContext) -> None:
+    """Обробка вибору словника для тренування."""
+    data_fsm: dict[str, Any] = await state.get_data()
+    vocab_id = int(callback.data.split('_')[-1])
+
+    await state.update_data(vocab_id=vocab_id)  # Додавання ID обраного словника
+
+    with Session() as db:
+        vocab_details: list[dict] = get_wordpairs_by_vocab_id(db, vocab_id)
+        user_vocab: dict = get_user_vocab_by_vocab_id(db, vocab_id)
+
+    vocab_name: str = user_vocab.name
+    vocab_note: str = user_vocab.description
+
+    current_state: Any | None = data_fsm.get('current_state')  # Поточний стан FSM
+
+    # Якщо був викликаний список словників з розділу бази словників
+    if current_state == 'VocabBaseState':
+        kb: InlineKeyboardMarkup = get_inline_kb_vocab_options()
+        msg_finally: str = (
+            f'Назва словника: {vocab_name}\n'
+            f'Примітка: {vocab_note or 'Відсутня'}\n'
+            f'Кількість словникових пар: {len(vocab_details)}\n\n'
+            f'Словникові пари:\n')
+
+        for idx, wordpairs_data in enumerate(vocab_details, start=1):
+            words_lst: list[tuple[str, str]] = wordpairs_data['words']
+            translations_lst: list[tuple[str, str]] = wordpairs_data['translations']
+            annotation: str = wordpairs_data['annotation'] or 'Немає анотації'
+
+            words_part: str = ', '.join([f'{word} [{transcription}]'
+                                    if transcription else word
+                                    for word, transcription in words_lst])
+            translations_part: str = ', '.join([f'{translation} [{transcription}]'
+                                        if transcription else translation
+                                        for translation, transcription in translations_lst])
+
+            msg_finally += f'{idx}. {words_part} : {translations_part} : {annotation}\n'
+    # Якщо був викликаний список словників з розділу тренування
+    elif current_state == 'VocabTrainState':
+        msg_finally: str = f'Ви обрали словник: {vocab_name}\nОберіть, будь-ласка, тип тренування.'
+        kb: InlineKeyboardMarkup = get_inline_kb_all_training()
+
+    await callback.message.edit_text(text=msg_finally, reply_markup=kb)
+'''
