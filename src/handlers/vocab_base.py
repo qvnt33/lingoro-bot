4caa228d5a30c20ceed930c55f3280f5d95b7195
qx3kk <romanchuk.up@gmail.com>
2024-11-19 00:22:22 +0200

    refactor: дороби реалізацію видалення словника

--- src/handlers/vocab_base.py
+++ src/handlers/vocab_base.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 
 from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
@@ -8,9 +9,20 @@ from db.crud import VocabCRUD, WordpairCRUD
 from db.database import Session
 from exceptions import InvalidVocabIndexError
 from src.filters.check_empty_filters import CheckEmptyFilter
-from src.keyboards.vocab_base_kb import get_inline_kb_vocab_base
+from src.keyboards.vocab_base_kb import (
+    get_inline_kb_accept_delete_vocab,
+    get_inline_kb_confirm_delete,
+    get_inline_kb_vocab_base,
+)
 from src.keyboards.vocab_selection_kb import get_inline_kb_vocab_selection
-from text_data import MSG_CHOOSE_VOCAB, MSG_ERROR_VOCAB_BASE_EMPTY, MSG_INFO_VOCAB, TEMPLATE_WORDPAIR
+from text_data import (
+    MSG_CHOOSE_VOCAB,
+    MSG_CONFIRM_DELETE_VOCAB,
+    MSG_ERROR_VOCAB_BASE_EMPTY,
+    MSG_INFO_VOCAB,
+    MSG_SUCCESS_VOCAB_DELETED,
+    TEMPLATE_WORDPAIR,
+)
 from tools.wordpair_utils import format_word_items
 
 router = Router(name='vocab_base')
@@ -18,12 +30,18 @@ logger: logging.Logger = logging.getLogger(__name__)
 
 
 @router.callback_query(F.data == 'vocab_base')
-async def process_vocab_base(callback: types.CallbackQuery) -> None:
+async def process_vocab_base(callback: types.CallbackQuery, state: FSMContext) -> None:
     """Відстежує натискання на кнопку "База словників" у головному меню.
     Відправляє користувачу словники у вигляді кнопок.
     """
     user_id: int = callback.from_user.id
+
+    logger.info(f'Користувач перейшов до розділу "База словників". USER_ID: {user_id}')
+
     check_empty_filter = CheckEmptyFilter()
+
+    await state.clear()
+    logger.info('FSM стан та FSM-Cache очищено')
 
     with Session() as session:
         vocab_crud = VocabCRUD(session)
@@ -31,25 +49,27 @@ async def process_vocab_base(callback: types.CallbackQuery) -> None:
         # Дані всіх користувацьких словників користувача
         all_vocabs_data: list[dict] = vocab_crud.get_all_vocabs_data(user_id)
 
-    # Якщо в БД користувача немає словників
+    # Якщо в БД користувача немає користувацьких словників
     if check_empty_filter.apply(all_vocabs_data):
+        logger.info('В БД користувача немає користувацьких словників')
         msg_text: str = MSG_ERROR_VOCAB_BASE_EMPTY
     else:
         msg_text: str = MSG_CHOOSE_VOCAB
 
-    kb: InlineKeyboardMarkup = get_inline_kb_vocab_selection(all_vocabs_data=all_vocabs_data,
-                                                             callback_prefix='select_vocab_base')
-
+    kb: InlineKeyboardMarkup = get_inline_kb_vocab_selection(all_vocabs_data, callback_prefix='select_vocab_base')
     await callback.message.edit_text(text=msg_text, reply_markup=kb)
 
 
 @router.callback_query(F.data.startswith('select_vocab_base'))
 async def process_vocab_base_selection(callback: types.CallbackQuery, state: FSMContext) -> None:
-    """Обробка вибору словника у базі словників"""
+    """Відстежує натискання на кнопку користувацького словника у розділі "База словників".
+    Відправляє користувачу його статистику з словниковими парами та клавіатуру для взаємодії з ним.
+    """
     vocab_id = int(callback.data.split('_')[-1])
+    logger.info(f'Був обраний користувацький словник у розділі "База словників". VOCAB_ID: {vocab_id}')
 
     await state.update_data(vocab_id=vocab_id)
-    logger.info('ID словника збережений у FSM-Cache')
+    logger.info('ID користувацького словника збережений у FSM-Cache')
 
     try:
         with Session() as session:
@@ -62,7 +82,7 @@ async def process_vocab_base_selection(callback: types.CallbackQuery, state: FSM
             sorted_wordpair_items: list[dict] = sorted(wordpair_items,
                                                        key=lambda item: item['number_errors'],
                                                        reverse=True)
-            vocab_data: dict = vocab_crud.get_vocab_data(vocab_id)
+            vocab_data: dict[str, Any] = vocab_crud.get_vocab_data(vocab_id)
     except InvalidVocabIndexError as e:
         logger.error(e)
         return
@@ -76,7 +96,7 @@ async def process_vocab_base_selection(callback: types.CallbackQuery, state: FSM
 
     formatted_wordpairs: list[str] = []
 
-    for idx, wordpair_item in enumerate(iterable=sorted_wordpair_items, start=1):
+    for idx, wordpair_item in enumerate(sorted_wordpair_items, start=1):
         word_items: list[dict] = wordpair_item.get('words')
         translation_items: list[dict] = wordpair_item.get('translations')
         annotation: str = wordpair_item.get('annotation') or 'Немає анотації'
@@ -101,3 +121,59 @@ async def process_vocab_base_selection(callback: types.CallbackQuery, state: FSM
                                                 wordpairs=joined_wordpairs)
 
     await callback.message.edit_text(text=msg_vocab_info, reply_markup=kb)
+
+
+@router.callback_query(F.data == 'delete_vocab')
+async def process_delete_vocab(callback: types.CallbackQuery, state: FSMContext) -> None:
+    """Відстежує натискання на кнопку "Видалити словник" після обрання користувацького словника
+    у розділі "База словників".
+    Відправляє клавіатуру для підтвердження видалення.
+    """
+    logger.info('Натиснуто кнопку "Видалити словник"')
+
+    data_fsm: dict[str, Any] = await state.get_data()
+    vocab_id: int | None = data_fsm.get('vocab_id')
+
+    try:
+        with Session() as session:
+            vocab_crud = VocabCRUD(session)
+            vocab_data: dict[str, Any] = vocab_crud.get_vocab_data(vocab_id)
+            vocab_name: str = vocab_data.get('name')
+    except InvalidVocabIndexError as e:
+        logger.error(e)
+        return
+
+    kb: InlineKeyboardMarkup = get_inline_kb_confirm_delete()
+    msg_confirm_delete_vocab: str = MSG_CONFIRM_DELETE_VOCAB.format(name=vocab_name)
+
+    await callback.message.edit_text(text=msg_confirm_delete_vocab, reply_markup=kb)
+
+
+@router.callback_query(F.data == 'accept_delete_vocab')
+async def process_accept_delete_vocab(callback: types.CallbackQuery, state: FSMContext) -> None:
+    """Відстежує натискання на кнопку "Так" при підтвердженні видалення користувацького словника.
+    Видаляє користувацький словник та всі його звʼязки з БД (слова та переклади залишаються в БД).
+    """
+    logger.info('Натиснуто кнопку "Так" під час підтвердження видалення словника')
+
+    data_fsm: dict[str, Any] = await state.get_data()
+    vocab_id: int | None = data_fsm.get('vocab_id')
+
+    try:
+        with Session() as session:
+            vocab_crud = VocabCRUD(session)
+
+            vocab_data: dict[str, Any] = vocab_crud.get_vocab_data(vocab_id)
+            vocab_name: str | None = vocab_data.get('name')
+
+            vocab_crud.delete_vocab(vocab_id)
+
+            logger.info('Словник був видалений з БД')
+    except InvalidVocabIndexError as e:
+        logger.error(e)
+        return
+
+    kb: InlineKeyboardMarkup = get_inline_kb_accept_delete_vocab()
+    msg_vocab_deleted: str = MSG_SUCCESS_VOCAB_DELETED.format(name=vocab_name)
+
+    await callback.message.edit_text(text=msg_vocab_deleted, reply_markup=kb)
--- src/keyboards/vocab_base_kb.py
+++ src/keyboards/vocab_base_kb.py
@@ -10,3 +10,19 @@ def get_inline_kb_vocab_base() -> InlineKeyboardMarkup:
         [InlineKeyboardButton(text='Головне меню', callback_data='menu')]]
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+def get_inline_kb_confirm_delete() -> InlineKeyboardMarkup:
+    buttons: list[list[InlineKeyboardButton]] = [
+        [InlineKeyboardButton(text='✅ Так', callback_data='accept_delete_vocab')],
+        [InlineKeyboardButton(text='❌ Ні', callback_data='vocab_base')]]
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+
+def get_inline_kb_accept_delete_vocab() -> InlineKeyboardMarkup:
+    buttons: list[list[InlineKeyboardButton]] = [
+        [InlineKeyboardButton(text='База словників', callback_data='vocab_base')],
+        [InlineKeyboardButton(text='Головне меню', callback_data='menu')]]
+
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
