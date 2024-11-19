e91f6fb8e49c93204982ed8472cc68c76b7d9608
qx3kk <romanchuk.up@gmail.com>
2024-11-19 22:15:40 +0200

    refactor: реалізуй початкову версію тренування

--- src/handlers/vocab_trainer.py
+++ src/handlers/vocab_trainer.py
@@ -1,46 +1,114 @@
+import logging
 import random
 from typing import Any, Dict, List
 
-from aiogram import F, Router
+from aiogram import F, Router, types
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, InlineKeyboardMarkup, Message
-from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
-from db.crud import get_wordpairs_by_vocab_id
+from db.crud import VocabCRUD, WordpairCRUD
 from db.database import Session
 from db.models import User
+from exceptions import InvalidVocabIndexError
+from src.filters.check_empty_filters import CheckEmptyFilter
 from src.fsm.states import VocabTraining
-from src.keyboards.vocab_trainer_kb import get_inline_kb_all_training, get_inline_kb_process_training
-from text_data import MSG_ERROR_VOCAB_BASE_EMPTY
+from src.keyboards.vocab_base_kb import get_inline_kb_vocab_base
+from src.keyboards.vocab_selection_kb import get_inline_kb_vocab_selection
+from src.keyboards.vocab_trainer_kb import get_inline_kb_all_training, get_inline_kb_process_training, get_inline_kb_vocab_selection_training
+from text_data import (
+    MSG_CHOOSE_TRAINING_TYPE,
+    MSG_CHOOSE_VOCAB_FOR_TRAINING,
+    MSG_ERROR_VOCAB_BASE_EMPTY,
+    MSG_ERROR_VOCAB_BASE_EMPTY_FOR_TRAINING,
+    MSG_INFO_VOCAB,
+    TEMPLATE_WORDPAIR,
+)
+from tools.wordpair_utils import format_word_items
 
 router = Router(name='vocab_trainer')
+logger: logging.Logger = logging.getLogger(__name__)
 
 
 @router.callback_query(F.data == 'vocab_trainer')
-async def process_vocab(callback: CallbackQuery, state: FSMContext) -> None:
-    """Відстежує натискання на кнопку "Тренування"""
+async def process_vocab_base(callback: types.CallbackQuery, state: FSMContext) -> None:
+    """Відстежує натискання на кнопку "Тренування" у головному меню.
+    Відправляє користувачу користувацькі словники у вигляді кнопок.
+    """
     user_id: int = callback.from_user.id
 
-    with Session() as db:
-        get_user_vocab_by_user_id = 1 # !trash
-        vocab_details: dict = get_user_vocab_by_user_id(db, user_id)
-        user_vocabs: User | None = get_user_vocab_by_user_id(db, user_id, is_all=True)  # Словники користувача
+    logger.info(f'Користувач перейшов до розділу "Тренування". USER_ID: {user_id}')
 
-        is_vocab_base_empty: bool = get_user_vocab_by_user_id(db, user_id) is None
+    check_empty_filter = CheckEmptyFilter()
 
-        if is_vocab_base_empty:
-            msg_finally: str = MSG_ERROR_VOCAB_BASE_EMPTY
-        else:
-            vocab_name: str = vocab_details.name
-            msg_finally: str = f'Ви обрали словник: "{vocab_name}"\nОберіть тип, будь-ласка, тип тренування.'
+    await state.clear()
+    logger.info('FSM стан та FSM-Cache очищено')
 
-    # Клавіатура для відображення словників
-    # kb: InlineKeyboardMarkup = get_inline_kb_vocab_selection(user_vocabs, is_with_create_vocab=False)
+    with Session() as session:
+        vocab_crud = VocabCRUD(session)
 
-    current_state = 'VocabTrainState'  # Стан FSM
-    await state.update_data(current_state=current_state)  # Збереження поточного стану FSM
+        # Дані всіх користувацьких словників користувача
+        all_vocabs_data: list[dict] = vocab_crud.get_all_vocabs_data(user_id)
 
-    # await callback.message.edit_text(text=msg_finally, reply_markup=kb)
+    # Якщо в БД користувача немає користувацьких словників
+    if check_empty_filter.apply(all_vocabs_data):
+        logger.info('В БД користувача немає користувацьких словників')
+        msg_text: str = MSG_ERROR_VOCAB_BASE_EMPTY_FOR_TRAINING
+        kb: InlineKeyboardMarkup = get_inline_kb_vocab_selection_training(all_vocabs_data,
+                                                             callback_prefix='select_vocab_training',
+                                                             is_with_btn_vocab_base=True)
+    else:
+        msg_text: str = MSG_CHOOSE_VOCAB_FOR_TRAINING
+
+        kb: InlineKeyboardMarkup = get_inline_kb_vocab_selection_training(all_vocabs_data,
+                                                                callback_prefix='select_vocab_training')
+    await callback.message.edit_text(text=msg_text, reply_markup=kb)
+
+
+@router.callback_query(F.data.startswith('select_vocab_training'))
+async def process_training_selection(callback: types.CallbackQuery, state: FSMContext) -> None:
+    """Відстежує натискання на кнопку користувацького словника у розділі "Тренування".
+    Відправляє клавіатуру з вибором типу тренування.
+    """
+    vocab_id = int(callback.data.split('_')[-1])
+    logger.info(f'Був обраний користувацький словник у розділі "Тренування". VOCAB_ID: {vocab_id}')
+
+    await state.update_data(vocab_id=vocab_id)
+    logger.info('ID користувацького словника збережений у FSM-Cache')
+
+    try:
+        with Session() as session:
+            vocab_crud = VocabCRUD(session)
+            vocab_data: dict[str, Any] = vocab_crud.get_vocab_data(vocab_id)
+
+            wordpair_crud = WordpairCRUD(session)
+            wordpair_items: list[dict] = wordpair_crud.get_wordpairs(vocab_id)
+    except InvalidVocabIndexError as e:
+        logger.error(e)
+        return
+
+    kb: InlineKeyboardMarkup = get_inline_kb_all_training()
+    vocab_name: str = vocab_data.get('name')
+    msg_choose_training_type: str = MSG_CHOOSE_TRAINING_TYPE.format(name=vocab_name)
+
+    wordpairs_count: int = vocab_data.get('wordpairs_count')
+
+    # Список індексів, які ще не були використані
+    available_idxs = list(range(wordpairs_count))
+
+    await state.update_data(vocab_data=vocab_data,
+                            wordpair_items=wordpair_items,
+                            available_idxs=available_idxs)
+
+    await callback.message.edit_text(text=msg_choose_training_type, reply_markup=kb)
+
+
+@router.callback_query(F.data == 'direct_translation')
+async def process_btn_direct_translation(callback: CallbackQuery, state: FSMContext) -> None:
+    """Ініціалізація тренування і збереження даних у FSM"""
+    await state.update_data(current_training_type='direct_translation')
+
+    # Надсилаємо першу пару на переклад
+    await send_next_word(callback.message, state)
 
 
 @router.callback_query(F.data == 'reverse_translation')
@@ -53,29 +121,10 @@ async def process_btn_reverse_translation(callback: CallbackQuery, state: FSMCon
         vocab_details: List[Dict] = get_wordpairs_by_vocab_id(db, vocab_id)
 
     # Список індексів, які ще не були використані
-    available_indices = list(range(len(vocab_details)))
+    available_idxs = list(range(len(vocab_details)))
 
     # Збереження всіх деталей у стані FSM
-    await state.update_data(vocab_details=vocab_details, available_indices=available_indices, flag_training='reverse_translation')
-
-    # Надсилаємо першу пару на переклад
-    await send_next_word(callback.message, state)
-
-
-@router.callback_query(F.data == 'direct_translation')
-async def process_btn_direct_translation(callback: CallbackQuery, state: FSMContext) -> None:
-    """Ініціалізація тренування і збереження даних у FSM"""
-    data_fsm: Dict[str, Any] = await state.get_data()
-    vocab_id: int = data_fsm.get('vocab_id')
-
-    with Session() as db:
-        vocab_details: List[Dict] = get_wordpairs_by_vocab_id(db, vocab_id)
-
-    # Список індексів, які ще не були використані
-    available_indices = list(range(len(vocab_details)))
-
-    # Збереження всіх деталей у стані FSM
-    await state.update_data(vocab_details=vocab_details, available_indices=available_indices, flag_training='direct_translation')
+    await state.update_data(vocab_details=vocab_details, available_idxs=available_idxs, current_training_type='reverse_translation')
 
     # Надсилаємо першу пару на переклад
     await send_next_word(callback.message, state)
@@ -83,23 +132,24 @@ async def process_btn_direct_translation(callback: CallbackQuery, state: FSMCont
 
 async def send_next_word(message: Message, state: FSMContext) -> None:
     """Надсилає наступне слово для перекладу"""
-    data_fsm: Dict[str, Any] = await state.get_data()
+    data_fsm: dict[str, Any] = await state.get_data()
 
-    vocab_details: List[Dict] = data_fsm.get('vocab_details')
-    available_indices = data_fsm.get('available_indices', [])  # Список індексів, які ще не були використані
-    old_wordpair_idx = data_fsm.get('wordpair_idx', 1)  # Минулий індекс
-    flag_training = data_fsm.get('flag_training')  # Флаг тренування
+    vocab_data: dict[str, Any] = data_fsm.get('vocab_data')
+    wordpair_items: list[dict] = data_fsm.get('wordpair_items')
 
-    # Вибір випадкового індексу з тих, що ще не використані
-    if len(available_indices) == 1:
-        wordpair_idx = available_indices[0]
-    elif len(available_indices) >= 1:
-        while True:
-            wordpair_idx = random.choice(available_indices)  # Індекс словникової пари
-            # Якщо індекс не такий як минулий
-            if wordpair_idx != old_wordpair_idx:
-                await state.update_data(available_indices=available_indices)  # Оновлення індексу
-                break
+    available_idxs: list = data_fsm.get('available_idxs', [])  # Список індексів, які ще не були використані
+    preview_wordpair_idx: int = data_fsm.get('wordpair_idx', 0)  # Минулий індекс
+    current_training_type: str = data_fsm.get('current_training_type')  # Тип тренування
+    wordpair_idx = 0
+
+    # Вибір випадкового індексу з тих, що ще не були використані
+    if len(available_idxs) == 1:
+        wordpair_idx: int = available_idxs[0]
+    elif len(available_idxs) > 1:
+        # Оновлення індексу (щоб не збігався з минулим)
+        while wordpair_idx == preview_wordpair_idx:
+            wordpair_idx: int = random.choice(available_idxs)  # Індекс словникової пари
+        await state.update_data(wordpair_idx=wordpair_idx)
     else:
         kb: InlineKeyboardMarkup = get_inline_kb_all_training()
         await message.answer(text='Всі словникові пари переведені. Тренування завершено!', reply_markup=kb)
@@ -108,15 +158,28 @@ async def send_next_word(message: Message, state: FSMContext) -> None:
         await state.update_data(vocab_id=vocab_id)  # Оновлення індексу
         return
 
-    wordpair_data = vocab_details[wordpair_idx]
-    if flag_training == 'direct_translation':
-        formatted_word = ', '.join(f'{word} [{transcription}]' if transcription else word for word, transcription in wordpair_data['words'])
-        formatted_translation = ', '.join(f'{word} [{transcription}]' if transcription else word for word, transcription in wordpair_data['translations'])
-        correct_translations = [translation[0].lower() for translation in wordpair_data['translations']]
-    elif flag_training == 'reverse_translation':
-        formatted_translation = ', '.join(f'{word} [{transcription}]' if transcription else word for word, transcription in wordpair_data['words'])
-        formatted_word = ', '.join(f'{word} [{transcription}]' if transcription else word for word, transcription in wordpair_data['translations'])
-        correct_translations = [translation[0].lower() for translation in wordpair_data['words']]
+    wordpair_data: dict = wordpair_items[wordpair_idx]
+    wordpair_words = wordpair_data.get('words')
+    wordpair_translations = wordpair_data.get('translations')
+    wordpair_annotation = wordpair_data.get('annotation')
+    wordpair_number_errors = wordpair_data.get('number_errors')
+
+    if current_training_type == 'direct_translation':
+        formatted_word = ', '.join(f'{word_item.get("word")} [{word_item.get("transcription")}]'
+                                   if word_item.get('transcription') else word_item.get('word')
+                                   for word_item in wordpair_words)
+        formatted_translation = ', '.join(f'{translation_item.get("translation")} [{translation_item.get("transcription")}]'
+                                          if translation_item.get('transcription') else translation_item.get('translation')
+                                          for translation_item in wordpair_translations)
+        correct_translations = [translation.get('translation').lower() for translation in wordpair_translations]
+    elif current_training_type == 'reverse_translation':
+        formatted_translation = ', '.join(f'{word_item.get("word")} [{word_item.get("transcription")}]'
+                                          if word_item.get('transcription') else word_item.get('word')
+                                          for word_item in wordpair_words)
+        formatted_word = ', '.join(f'{translation_item.get("translation")} [{translation_item.get("transcription")}]'
+                                   if translation_item.get('transcription') else translation_item.get('translation')
+                                   for translation_item in wordpair_translations)
+        correct_translations = [translation.get('word').lower() for translation in wordpair_words]
 
     await state.update_data(formatted_word=formatted_word,
                             formatted_translation=formatted_translation,
@@ -135,17 +198,13 @@ async def process_translation(message: Message, state: FSMContext) -> None:
     """Обробляє переклад, введений користувачем"""
     data_fsm: Dict[str, Any] = await state.get_data()
 
-    vocab_details: List[Dict] = data_fsm.get('vocab_details')
     wordpair_idx = data_fsm.get('wordpair_idx')
 
     formatted_word = data_fsm.get('formatted_word')
     formatted_translation = data_fsm.get('formatted_translation')
 
-    available_indices = data_fsm.get('available_indices', [])  # Список індексів, які ще не були використані
+    available_idxs = data_fsm.get('available_idxs', [])  # Список індексів, які ще не були використані
     correct_translations = data_fsm.get('correct_translations')
-
-    wordpair_data = vocab_details[wordpair_idx]
-    kb: InlineKeyboardMarkup = get_inline_kb_process_training()  # Клавіатура для тренування
 
     # Переклади без анотацій
     print(correct_translations)
@@ -153,8 +212,8 @@ async def process_translation(message: Message, state: FSMContext) -> None:
     print(user_translation)
     if user_translation in correct_translations:
         await message.answer(f'Правильно!\n{formatted_word} -> {formatted_translation}')
-        available_indices.remove(wordpair_idx)
-        await state.update_data(available_indices=available_indices)  # Оновлення індексу
+        available_idxs.remove(wordpair_idx)
+        await state.update_data(available_idxs=available_idxs)  # Оновлення індексу
         await send_next_word(message, state)
     else:
         await message.answer('Не Правильно!')
@@ -165,10 +224,13 @@ async def process_translation(message: Message, state: FSMContext) -> None:
 async def process_cancel_training(callback: CallbackQuery, state: FSMContext) -> None:
     """Відстежує натискання на кнопку "Скасувати" під час тренування"""
     data_fsm: Dict[str, Any] = await state.get_data()
-    vocab_id: int = data_fsm.get('vocab_id')
+    # vocab_id: int = data_fsm.get('vocab_id')
     await state.clear()  # Очищення FSM-кеш та FSM-стану
-    await state.update_data(vocab_id=vocab_id)  # Оновлення індексу
-    await process_vocab(callback, state)
+    # await state.update_data(vocab_id=vocab_id)  # Оновлення індексу
+    kb: InlineKeyboardMarkup = get_inline_kb_all_training()
+    vocab_name: str = 'vocab_data.get(name)'
+    msg_choose_training_type: str = MSG_CHOOSE_TRAINING_TYPE.format(name=vocab_name)
+    await callback.message.edit_text(text=msg_choose_training_type, reply_markup=kb)
 
 
 
--- src/keyboards/vocab_trainer_kb.py
+++ src/keyboards/vocab_trainer_kb.py
@@ -35,3 +35,42 @@ def get_inline_kb_process_training() -> InlineKeyboardMarkup:
     inline_builder.row(btn_cancel)
 
     return inline_builder.as_markup()
+
+
+def get_inline_kb_vocab_selection_training(all_vocabs_data: list[dict],
+                                  callback_prefix: str,
+                                  is_with_btn_vocab_base: bool = False) -> InlineKeyboardMarkup:
+    """Клавіатура з вибором словників.
+
+    Args:
+        all_vocabs_data (list[dict]): Список словників зі всіма даними.
+        callback_prefix (str): Префікс для callback_data кнопок, залежить від контексту.
+        is_for_training (bool): Додавання кнопки "База словників" для розділу "Тренування" та кнопки
+        "Додати словник" для розділу "База словників"
+
+    Returns:
+        InlineKeyboardMarkup: Сформована клавіатура.
+    """
+    kb = InlineKeyboardBuilder()
+
+    # Генерація кнопок для кожного словника
+    for vocab in all_vocabs_data[::-1]:
+        vocab_id: int = vocab['id']
+        vocab_name: str = vocab['name']
+        wordpairs_count: int = vocab['wordpairs_count']
+
+        btn_text: str = f'{vocab_name} [{wordpairs_count}]'
+        callback_data_text: str = f'{callback_prefix}_{vocab_id}'
+
+        btn_vocab = InlineKeyboardButton(text=btn_text, callback_data=callback_data_text)
+        kb.add(btn_vocab)
+
+    if is_with_btn_vocab_base:
+        btn_vocab_base = InlineKeyboardButton(text='База словників', callback_data='vocab_base')
+        kb.add(btn_vocab_base)
+
+    btn_menu = InlineKeyboardButton(text='Головне меню', callback_data='menu')
+    kb.add(btn_menu)
+
+    kb.adjust(1)
+    return kb.as_markup()
