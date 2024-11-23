bc77a1dc6e143de68ceb2ed694cff9cb67f5123a
qx3kk <romanchuk.up@gmail.com>
2024-11-23 04:51:08 +0200

    refactor: дороби docstring для клавіатур

--- src/keyboards/help_kb.py
+++ src/keyboards/help_kb.py
@@ -1,15 +1,9 @@
 from aiogram.types import InlineKeyboardButton
 from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def get_kb_help() -> InlineKeyboardMarkup:
-    """Клавіатура з кнопкою головного меню"""
-    inline_builder = InlineKeyboardBuilder()
-
-    btn_menu = InlineKeyboardButton(text='🏠 Головне меню',
-                                    callback_data='menu')
-
-    inline_builder.row(btn_menu)
-
-    return inline_builder.as_markup()
+    """Повертає клавіатуру для розділу "Справка" з кнопкою головного меню"""
+    buttons: list[list[InlineKeyboardButton]] = [
+        [InlineKeyboardButton(text='🏠 Головне меню', callback_data='menu')]]
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
--- src/keyboards/menu_kb.py
+++ src/keyboards/menu_kb.py
@@ -3,9 +3,9 @@ from aiogram.types.inline_keyboard_markup import InlineKeyboardMarkup
 
 
 def get_kb_menu() -> InlineKeyboardMarkup:
+    """Повертає клавіатуру для розділу "Головне меню" з розділами"""
     buttons: list[list[InlineKeyboardButton]] = [
         [InlineKeyboardButton(text='📚 Словниковий тренажер', callback_data='vocab_trainer')],
         [InlineKeyboardButton(text='📂 База словників', callback_data='vocab_base')],
         [InlineKeyboardButton(text='⁉️ Довідка', callback_data='help')]]
-
     return InlineKeyboardMarkup(inline_keyboard=buttons)
